53d5d0d754d04afae31dcf0f0d01a58390951224
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2024-06-11 09:42:32 +0700

    DEMO angular dashboard

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +6,18 @@ import { Router } from '@angular/router';
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  currentAccount = localStorage.getItem('currentAccount');
+  profileData:any;
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    var listAccount = JSON.parse(localStorage.getItem('user')as string)||<any>[];
+    listAccount.forEach((item:any) => {
+      if(this.currentAccount == item.email){
+        this.profileData = item;
+      }
+    });
+  }
   logOut() {
     this.router.navigate(['/', 'account', 'auth', 'login']);
     localStorage.removeItem('isLogin');
